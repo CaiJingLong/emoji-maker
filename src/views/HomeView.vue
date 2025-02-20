926a24939b1146726b9f768517b1bafcea1d1f94
CaiJingLong <cjl_spy@163.com>
2025-02-20 21:06:54 +0800

    Clean up project structure and remove default Vue template components
    - Removed unnecessary default Vue components (HelloWorld, TheWelcome, WelcomeItem)
    - Deleted icon components in the icons directory
    - Updated HomeView to use EmojiMaker component directly
    - Removed HelloWorld test file
    - Simplified project components to focus on core functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import EmojiMaker from '../components/EmojiMaker.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <EmojiMaker />
   </main>
 </template>
